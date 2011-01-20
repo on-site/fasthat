b7e02beb824110f581a3ba542d142a2c48bc92b4
Chris Jester-Young <cky@on-site.com>
2011-01-20 13:22:50 -0500

    Implement histogram-by-referee.
    This feature is similar to refsByType, but has three advantages:
    
    1. You can access it straight from the histogram page, rather than
       having to do it via the class page (which adds an extra click---try
       it and see how much that extra click discourages its use :-P).
    
    2. The results are in the histogram page, so exploring high-frequency
       reference chains becomes a very interactive process, especially
       when using the default sorting order (refCount).
    
    3. It calculates the reference count more usefully. With refsByType, if
       multiple instances of a given class all refer to the _same_ object,
       the ref count goes up by the instance count. With the new version,
       the ref count goes up by 1; after all, those instances are all
       referring to the same one object. This allows you to quickly tell
       _how many_ instances are being hogged up by a certain class.

--- src/com/sun/tools/hat/internal/server/HistogramQuery.java
+++ src/com/sun/tools/hat/internal/server/HistogramQuery.java
@@ -33,9 +33,19 @@
 package com.sun.tools.hat.internal.server;
 
 import com.google.common.base.Function;
+import com.google.common.collect.ImmutableMultimap;
+import com.google.common.collect.ImmutableMultiset;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.ImmutableSetMultimap;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Multimaps;
 import com.google.common.collect.Ordering;
 import com.sun.tools.hat.internal.model.JavaClass;
+import com.sun.tools.hat.internal.model.JavaHeapObject;
+import com.sun.tools.hat.internal.model.Snapshot;
+
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Comparator;
 
 /**
@@ -49,20 +59,6 @@ public class HistogramQuery extends QueryHandler {
             @Override
             public String apply(JavaClass clazz) {
                 return clazz.getName();
-            }
-        },
-
-        BY_INSTANCES_COUNT {
-            @Override
-            public Integer apply(JavaClass clazz) {
-                return ~clazz.getInstancesCount(false);
-            }
-        },
-
-        BY_TOTAL_INSTANCE_SIZE {
-            @Override
-            public Long apply(JavaClass clazz) {
-                return ~clazz.getTotalInstanceSize();
             }
         };
 
@@ -78,42 +74,210 @@ public class HistogramQuery extends QueryHandler {
         }
     }
 
+    private static abstract class MetricsProvider {
+        private final JavaClass[] classes;
+
+        protected MetricsProvider(JavaClass[] classes) {
+            this.classes = classes;
+        }
+
+        public abstract int getCount(JavaClass clazz);
+        public abstract long getSize(JavaClass clazz);
+        public int getRefCount(JavaClass clazz) {throw new UnsupportedOperationException();}
+
+        /*
+         * If Java had first-class methods, these functions would not be
+         * necessary. :-(
+         */
+        public Function<JavaClass, Integer> getCountMethod() {
+            return new Function<JavaClass, Integer>() {
+                public Integer apply(JavaClass clazz) {return getCount(clazz);}
+            };
+        }
+        public Function<JavaClass, Long> getSizeMethod() {
+            return new Function<JavaClass, Long>() {
+                public Long apply(JavaClass clazz) {return getSize(clazz);}
+            };
+        }
+        public Function<JavaClass, Integer> getRefCountMethod() {
+            return new Function<JavaClass, Integer>() {
+                public Integer apply(JavaClass clazz) {return getRefCount(clazz);}
+            };
+        }
+ 
+        public JavaClass[] getClasses() {return classes.clone();}
+        public boolean hasRefCount() {return false;}
+    }
+
+    private static class GlobalMetricsProvider extends MetricsProvider {
+        public GlobalMetricsProvider(Snapshot snapshot) {
+            super(snapshot.getClassesArray());
+        }
+
+        @Override
+        public int getCount(JavaClass clazz) {
+            return clazz.getInstancesCount(false);
+        }
+
+        @Override
+        public long getSize(JavaClass clazz) {
+            return clazz.getTotalInstanceSize();
+        }
+    }
+
+    private static class RefereeMetricsProvider extends MetricsProvider {
+        private enum GetClass implements Function<JavaHeapObject, JavaClass> {
+            INSTANCE;
+    
+            @Override
+            public JavaClass apply(JavaHeapObject obj) {
+                return obj.getClazz();
+            }
+        }
+
+        final ImmutableMultimap<JavaClass, JavaHeapObject> referrers;
+        final ImmutableMultiset<JavaClass> references;
+
+        public RefereeMetricsProvider(JavaClass referee) {
+            this(Multimaps.index(getInstanceReferrers(referee), GetClass.INSTANCE),
+                    getReferencesByClass(referee).keys());
+        }
+
+        private RefereeMetricsProvider(ImmutableMultimap<JavaClass, JavaHeapObject> referrers,
+                ImmutableMultiset<JavaClass> references) {
+            super(referrers.keySet().toArray(new JavaClass[0]));
+            this.referrers = referrers;
+            this.references = references;
+        }
+
+        @Override
+        public int getCount(JavaClass clazz) {
+            return referrers.get(clazz).size();
+        }
+
+        @Override
+        public long getSize(JavaClass clazz) {
+            Collection<JavaHeapObject> subset = referrers.get(clazz);
+            if (!clazz.isArray()) {
+                return (long) clazz.getInstanceSize() * subset.size();
+            }
+            long size = 0;
+            for (JavaHeapObject instance : subset) {
+                size += instance.getSize();
+            }
+            return size;
+        }
+
+        @Override
+        public int getRefCount(JavaClass clazz) {
+            return references.count(clazz);
+        }
+
+        @Override
+        public boolean hasRefCount() {
+            return true;
+        }
+
+        private static ImmutableSet<JavaHeapObject> getInstanceReferrers(JavaClass clazz) {
+            ImmutableSet.Builder<JavaHeapObject> builder = ImmutableSet.builder();
+            for (JavaHeapObject instance : clazz.getInstances(false)) {
+                builder.addAll(instance.getReferers());
+            }
+            return builder.build();
+        }
+    
+        private static ImmutableMultimap<JavaClass, JavaHeapObject> getReferencesByClass(
+                JavaClass clazz) {
+            ImmutableSetMultimap.Builder<JavaClass, JavaHeapObject> builder = ImmutableSetMultimap.builder();
+            for (JavaHeapObject instance : clazz.getInstances(false)) {
+                for (JavaHeapObject referrer : instance.getReferers()) {
+                    builder.put(referrer.getClazz(), instance);
+                }
+            }
+            return builder.build();
+        }
+    }
+
     public void run() {
-        JavaClass[] classes = snapshot.getClassesArray();
+        String referee = Iterables.getOnlyElement(params.get("referee"), null);
+        JavaClass refereeClass;
+        MetricsProvider metrics;
+        if (referee != null) {
+            refereeClass = snapshot.findClass(referee);
+            if (refereeClass == null) {
+                error("class not found: " + referee);
+                return;
+            }
+            metrics = new RefereeMetricsProvider(refereeClass);
+        } else {
+            refereeClass = null;
+            metrics = new GlobalMetricsProvider(snapshot);
+        }
+
         Comparator<JavaClass> comparator;
         if (query.equals("count")) {
-            comparator = Sorters.BY_INSTANCES_COUNT;
+            comparator = Ordering.natural().reverse().onResultOf(metrics.getCountMethod());
         } else if (query.equals("class")) {
             comparator = Sorters.BY_NAME;
+        } else if (query.equals("size") || !metrics.hasRefCount()) {
+            comparator = Ordering.natural().reverse().onResultOf(metrics.getSizeMethod());
         } else {
-            // default sort is by total size
-            comparator = Sorters.BY_TOTAL_INSTANCE_SIZE;
+            comparator = Ordering.natural().reverse().onResultOf(metrics.getRefCountMethod());
         }
+        JavaClass[] classes = metrics.getClasses();
         Arrays.sort(classes, comparator);
 
         startHtml("Heap Histogram");
 
+        if (refereeClass != null) {
+            out.println("<p align='center'>");
+            printClass(refereeClass);
+            if (refereeClass.getId() != -1) {
+                out.println("[" + refereeClass.getIdString() + "]");
+            }
+            out.println("</p>");
+        }
         out.println("<p align='center'>");
         out.println("<b><a href='/'>All Classes (excluding platform)</a></b>");
         out.println("</p>");
 
         out.println("<table align=center border=1>");
-        out.println("<tr><th><a href='/histo/class'>Class</a></th>");
-        out.println("<th><a href='/histo/count'>Instance Count</a></th>");
-        out.println("<th><a href='/histo/size'>Total Size</a></th></tr>");
+        out.println("<tr>");
+        printHeader("class", "Class", refereeClass);
+        if (metrics.hasRefCount()) {
+            printHeader("refCount", "Reference Count", refereeClass);
+        }
+        printHeader("count", "Instance Count", refereeClass);
+        printHeader("size", "Total Size", refereeClass);
+        out.println("</tr>");
         for (JavaClass clazz : classes) {
-            out.println("<tr><td>");
+            out.print("<tr><td>");
             printClass(clazz);
-            out.println("</td>");
-            out.println("<td>");
-            out.println(clazz.getInstancesCount(false));
-            out.println("</td>");
-            out.println("<td>");
-            out.println(clazz.getTotalInstanceSize());
-            out.println("</td></tr>");
+            out.print(" (");
+            printLink(query, "histo", clazz);
+            out.println(")</td>");
+            if (metrics.hasRefCount()) {
+                out.printf("<td>%s</td>%n", metrics.getRefCount(clazz));
+            }
+            out.printf("<td>%s</td>%n", metrics.getCount(clazz));
+            out.printf("<td>%s</td></tr>%n", metrics.getSize(clazz));
         }
         out.println("</table>");
 
         endHtml();
+    }
+
+    private void printHeader(String pathInfo, String label, JavaClass referee) {
+        out.print("<th>");
+        printLink(pathInfo, label, referee);
+        out.println("</th>");
+    }
+
+    private void printLink(String pathInfo, String label, JavaClass referee) {
+        out.printf("<a href='/histo/%s", encodeForURL(pathInfo));
+        if (referee != null) {
+            out.printf("?referee=%s", encodeForURL(referee));
+        }
+        out.printf("'>%s</a>", label);
     }
 }
