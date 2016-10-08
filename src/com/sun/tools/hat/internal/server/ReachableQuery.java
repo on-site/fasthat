501fab8d28fe2e8c06b0306918f5fa258aeea317
Mike Virata-Stone <mjstone@on-site.com>
2016-10-08 10:23:25 -0700

    Mustachify ReachableQuery

--- src/com/sun/tools/hat/internal/server/ReachableQuery.java
+++ src/com/sun/tools/hat/internal/server/ReachableQuery.java
@@ -32,7 +32,7 @@
 
 package com.sun.tools.hat.internal.server;
 
-import com.sun.tools.hat.internal.model.*;
+import com.sun.tools.hat.internal.server.view.ReachableObjectsView;
 
 /**
  *
@@ -40,52 +40,8 @@ import com.sun.tools.hat.internal.model.*;
  */
 
 
-class ReachableQuery extends QueryHandler {
-        // We inherit printFullClass from ClassQuery
-
-
-    public ReachableQuery() {
-    }
-
-    @Override
-    public void run() {
-        startHtml("Objects Reachable From %s", query);
-        long id = parseHex(query);
-        JavaHeapObject root = snapshot.findThing(id);
-        ReachableObjects ro = new ReachableObjects(root,
-                                   snapshot.getReachableExcludes());
-        // Now, print out the sorted list, but start with root
-        long totalSize = ro.getTotalSize();
-        JavaThing[] things = ro.getReachables();
-        long instances = things.length;
-
-        out.print("<strong>");
-        printThing(root);
-        out.println("</strong><br>");
-        out.println("<br>");
-        for (JavaThing thing : things) {
-            printThing(thing);
-            out.println("<br>");
-        }
-
-        printFields(ro.getUsedFields(), "Data Members Followed");
-        printFields(ro.getExcludedFields(), "Excluded Data Members");
-        out.println("<h2>Total of " + instances + " instances occupying " + totalSize + " bytes.</h2>");
-
-        endHtml();
-    }
-
-    private void printFields(String[] fields, String title) {
-        if (fields.length == 0) {
-            return;
-        }
-        out.print("<h3>");
-        print(title);
-        out.println("</h3>");
-
-        for (String field : fields) {
-            print(field);
-            out.println("<br>");
-        }
+class ReachableQuery extends MustacheQueryHandler {
+    public ReachableObjectsView getReachableObjects() {
+        return new ReachableObjectsView(this, snapshot.findThing(parseHex(query)));
     }
 }
---
+++ src/com/sun/tools/hat/internal/server/view/ReachableObjectsView.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright © 2016 On-Site.com.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this work. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Linking this library statically or dynamically with other modules is
+ * making a combined work based on this library. Thus, the terms and
+ * conditions of the GNU General Public License cover the whole combination.
+ *
+ * As a special exception, the copyright holders of this library give you
+ * permission to link this library with independent modules to produce an
+ * executable, regardless of the license terms of these independent modules,
+ * and to copy and distribute the resulting executable under terms of your
+ * choice, provided that you also meet, for each linked independent module,
+ * the terms and conditions of the license of that module. An independent
+ * module is a module which is not derived from or based on this library.
+ * If you modify this library, you may extend this exception to your
+ * version of the library, but you are not obligated to do so. If you do
+ * not wish to do so, delete this exception statement from your version.
+ */
+package com.sun.tools.hat.internal.server.view;
+
+import com.sun.tools.hat.internal.model.JavaHeapObject;
+import com.sun.tools.hat.internal.model.ReachableObjects;
+import com.sun.tools.hat.internal.server.QueryHandler;
+import com.sun.tools.hat.internal.util.StreamIterable;
+
+import java.util.Arrays;
+
+/**
+ * View model for {@link ReachableObjects}.
+ *
+ * @author Mike Virata-Stone
+ */
+public class ReachableObjectsView extends ViewModel {
+    private final JavaThingView root;
+    private final ReachableObjects reachableObjects;
+
+    public ReachableObjectsView(QueryHandler handler, JavaHeapObject root) {
+        super(handler);
+        this.root = new JavaThingView(handler, root);
+        this.reachableObjects = new ReachableObjects(root, handler.getSnapshot().getReachableExcludes());
+    }
+
+    public JavaThingView getRoot() {
+        return root;
+    }
+
+    public Iterable<JavaThingView> getThings() {
+        return new StreamIterable<>(Arrays.asList(reachableObjects.getReachables()).stream()
+                .map(thing -> new JavaThingView(handler, thing)));
+    }
+
+    public boolean hasUsedFields() {
+        return getUsedFields().length > 0;
+    }
+
+    public String[] getUsedFields() {
+        return reachableObjects.getUsedFields();
+    }
+
+    public boolean hasExcludedFields() {
+        return getExcludedFields().length > 0;
+    }
+
+    public String[] getExcludedFields() {
+        return reachableObjects.getExcludedFields();
+    }
+
+    public long getNumInstances() {
+        return reachableObjects.getReachables().length;
+    }
+
+    public long getTotalSize() {
+        return reachableObjects.getTotalSize();
+    }
+}
